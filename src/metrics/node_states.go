f8ec69a2c712cdf4c0346141ca382a361bc22ef8
Nathan Pierce <connarpierce@gmail.com>
2021-01-08 16:59:45 -0500

    added anka_node_states

--- src/metrics/node_states.go
+++ src/metrics/node_states.go
@@ -9,12 +9,12 @@ import (
 // TODO: can we make prometheus.GaugeVec support also .Gauge?
 type NodeStatesMetric struct {
 	BaseAnkaMetric
-	HandleData func(types.Node, *prometheus.GaugeVec, prometheus.Labels)
+	HandleData func([]types.Node, *prometheus.GaugeVec)
 }
 
 func (this NodeStatesMetric) GetEventHandler() func(interface{}) error {
 	return func(d interface{}) error {
-		data, err := ConvertToNodeData(d)
+		nodesData, err := ConvertToNodeData(d)
 		if err != nil {
 			return err
 		}
@@ -22,14 +22,10 @@ func (this NodeStatesMetric) GetEventHandler() func(interface{}) error {
 		if err != nil {
 			return err
 		}
-		var stateIntMap = intMapFromStringSlice(types.NodeStates)
-		for _, nodeData := range data {
-			stateIntMap[nodeData.State] = stateIntMap[nodeData.State] + 1
-		}
-		for _, state := range types.NodeStates {
-			metric.With(prometheus.Labels{"state": state}).Set(float64(0))
-			metric.With(prometheus.Labels{"state": state}).Set(float64(stateIntMap[state]))
-		}
+		this.HandleData(
+			nodesData,
+			metric,
+		)
 		return nil
 	}
 }
@@ -40,8 +36,31 @@ var ankaNodeStatesMetrics = []NodeStatesMetric{
 			metric: CreateGaugeMetricVec("anka_node_states_count", "Count of Nodes in a particular State (label: state)", []string{"state"}),
 			event:  events.EVENT_NODE_UPDATED,
 		},
-		HandleData: func(nodeData types.Node, metric *prometheus.GaugeVec, labels prometheus.Labels) {
-			metric.With(labels).Set(float64(nodeData.VMCount))
+		HandleData: func(nodes []types.Node, metric *prometheus.GaugeVec) {
+			var stateIntMap = intMapFromStringSlice(types.NodeStates)
+			for _, node := range nodes {
+				stateIntMap[node.State] = stateIntMap[node.State] + 1
+			}
+			for _, state := range types.NodeStates {
+				metric.With(prometheus.Labels{"state": state}).Set(float64(stateIntMap[state]))
+			}
+		},
+	},
+	NodeStatesMetric{
+		BaseAnkaMetric: BaseAnkaMetric{
+			metric: CreateGaugeMetricVec("anka_node_states", "Node state (1 = current state) (label: id, name, state)", []string{"id", "name", "state"}),
+			event:  events.EVENT_NODE_UPDATED,
+		},
+		HandleData: func(nodes []types.Node, metric *prometheus.GaugeVec) {
+			for _, node := range nodes {
+				for _, state := range types.NodeStates {
+					if state == node.State {
+						metric.With(prometheus.Labels{"id": node.NodeID, "name": node.NodeName, "state": node.State}).Set(float64(1))
+					} else {
+						metric.With(prometheus.Labels{"id": node.NodeID, "name": node.NodeName, "state": state}).Set(float64(0))
+					}
+				}
+			}
 		},
 	},
 }
