60465faa1317e77e95b52dba8271b4d5a7021735
Nathan Pierce <connarpierce@gmail.com>
2021-04-22 12:48:54 -0400

    generate script improvement

--- generate-dockerhub-tags.bash
+++ generate-dockerhub-tags.bash
@@ -4,9 +4,9 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 NAME="anka-prometheus-exporter"
 DOCKERFILE_PATH="$SCRIPT_DIR/docker/scratch"
 LINUX_BINARY="$SCRIPT_DIR/bin/${NAME}_linux_amd64"
-[[ ! -f $LINUX_BINARY ]] && make build-linux
+rm -f $LINUX_BINARY
+make build-linux
 cp -f $LINUX_BINARY $DOCKERFILE_PATH/$NAME
 cd $DOCKERFILE_PATH
-# docker build --no-cache -t $NAME:latest .
 docker buildx build --platform linux/amd64,linux/arm64,linux/386 -t veertu/$NAME:latest -t veertu/$NAME:v$(cat $SCRIPT_DIR/VERSION) --push .
 rm -f "$DOCKERFILE_PATH/$NAME"
