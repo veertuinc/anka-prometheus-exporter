ea6dfe0df65f8ac2b770833603c6310d5c3d31f1
Nathan Pierce <connarpierce@gmail.com>
2021-07-13 09:42:40 -0400

    fix for dockerhub deploy script

--- generate-dockerhub-tags.bash
+++ generate-dockerhub-tags.bash
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -exo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-NAME="anka-prometheus-exporter"
 DOCKERFILE_PATH="$SCRIPT_DIR/docker/scratch"
-LINUX_BINARY="$SCRIPT_DIR/bin/${NAME}_linux_amd64"
+NAME="anka-prometheus-exporter"
+FULL_BINARY_NAME="${NAME}_linux_amd64"
+cleanup() {
+  rm -f ${DOCKERFILE_PATH}/${NAME}*
+}
+LINUX_BINARY="$SCRIPT_DIR/bin/${FULL_BINARY_NAME}"
 rm -f $LINUX_BINARY
 make build-linux
-cp -f $LINUX_BINARY $DOCKERFILE_PATH/$NAME
+cp -f $LINUX_BINARY $DOCKERFILE_PATH/
+trap cleanup EXIT
 cd $DOCKERFILE_PATH
 docker buildx build --platform linux/amd64,linux/arm64,linux/386 -t veertu/$NAME:latest -t veertu/$NAME:v$(cat $SCRIPT_DIR/VERSION) --push .
-rm -f "$DOCKERFILE_PATH/$NAME"
